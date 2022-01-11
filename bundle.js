50fec5d25c8dbcfcddc9f23f37a033fe52f3a01d
Jesse <jesse@piascik.net>
2022-01-11 14:10:30 -0500

    switch to nunjucks and decode output

--- bundle.js
+++ bundle.js
@@ -4,6 +4,7 @@ var _path = require('path');
 var Plugin = require('imdone-api');
 var settings = require('imdone-api/lib/settings');
 var Ecto = require('Ecto');
+var htmlEntities = require('html-entities');
 var fs = require('fs');
 
 function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
@@ -19,7 +20,7 @@ class PublishPlugin extends Plugin__default["default"] {
     this.ecto = new Ecto.Ecto();
     this.defaultTemplate = {
       name: 'basic board',
-      path: _path__default["default"].join('.imdone','plugins','imdone-publish-plugin','basic-board.mustache')
+      path: _path__default["default"].join('.imdone','plugins','imdone-publish-plugin','basic-board.njk')
     };
   }
   
@@ -36,7 +37,7 @@ class PublishPlugin extends Plugin__default["default"] {
             try {
               const source = await fs__default["default"].promises.readFile(filePath, "utf8");
               const content = await this.ecto.render(source, {path: this.project.path, lists: this.project.lists}, engineName);
-              this.project.copyToClipboard(content, `${name} copied to clipboard!`);
+              this.project.copyToClipboard(htmlEntities.decode(content), `${name} copied to clipboard!`);
             } catch (err) {
               console.log(`Error copying ${name} to clipboard`, err);
             }
@@ -54,7 +55,7 @@ class PublishPlugin extends Plugin__default["default"] {
           new settings.ArrayProperty()
             .itemsDraggable(true)
             .setTitle('Templates')
-            .setDescription('Add mustache templates')
+            .setDescription('Templates for copying current board.')
             .itemTitle('Template')
             .addItemProperty('name', new settings.StringProperty().setTitle('Name'))
             .addItemProperty('path', new settings.StringProperty().setTitle('Path'))
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import _path from 'path'
 import Plugin from 'imdone-api'
 import {Settings, ArrayProperty, StringProperty} from 'imdone-api/lib/settings'
-import { Ecto } from 'Ecto'
+import {Ecto} from 'Ecto'
+import {decode} from 'html-entities'
 import fs from 'fs'
 
 export default class PublishPlugin extends Plugin {
@@ -11,7 +12,7 @@ export default class PublishPlugin extends Plugin {
     this.ecto = new Ecto()
     this.defaultTemplate = {
       name: 'basic board',
-      path: _path.join('.imdone','plugins','imdone-publish-plugin','basic-board.mustache')
+      path: _path.join('.imdone','plugins','imdone-publish-plugin','basic-board.njk')
     }
   }
   
@@ -28,7 +29,7 @@ export default class PublishPlugin extends Plugin {
             try {
               const source = await fs.promises.readFile(filePath, "utf8")
               const content = await this.ecto.render(source, {path: this.project.path, lists: this.project.lists}, engineName)
-              this.project.copyToClipboard(content, `${name} copied to clipboard!`)
+              this.project.copyToClipboard(decode(content), `${name} copied to clipboard!`)
             } catch (err) {
               console.log(`Error copying ${name} to clipboard`, err)
             }
@@ -46,7 +47,7 @@ export default class PublishPlugin extends Plugin {
           new ArrayProperty()
             .itemsDraggable(true)
             .setTitle('Templates')
-            .setDescription('Add mustache templates')
+            .setDescription('Templates for copying current board.')
             .itemTitle('Template')
             .addItemProperty('name', new StringProperty().setTitle('Name'))
             .addItemProperty('path', new StringProperty().setTitle('Path'))
